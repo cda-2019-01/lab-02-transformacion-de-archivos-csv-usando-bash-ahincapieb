dcf9b912e07178ce70e136a7eb360f63c59c668e
Ana María <ahincapieb@unal.edu.co>
2019-05-09 22:37:29 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+sed 's/\//-/g' data.csv > data1.csv
+sed 's/-\([0-9][0-9]\);/-20\1;/' data1.csv > data2.csv
+sed 's|\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9][0-9][0-9]\);|\3-\2-\1;|g' data2.csv > data3.csv
+sed 's/\,/./g' data3.csv > data4.csv
+sed 's/\;/,/g' data4.csv > data5.csv
+sed -e 's:^,:/N,:g' -e 's:,,:,/N,:g' -e 's:,$:,/N:g' data5.csv > data6.csv
+sed '\:/N:d' data7.csv > datafinal.csv
