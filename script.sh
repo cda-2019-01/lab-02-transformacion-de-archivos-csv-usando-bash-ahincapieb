0560aba5ee738049abb51e5a790205057a0aaf5e
Ana María <ahincapieb@unal.edu.co>
2019-05-14 20:50:20 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+# Escriba su código aquí
+#!/bin/bash
 # Agregar nombre de la estación
 sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/estacion1;\1\/2\/3/g' estacion1.csv > estacion1a.csv
 sed 's/\([0-9]*\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/estacion2;\1\/2\/3/g' estacion2.csv > estacion2a.csv
@@ -5,19 +7,27 @@ sed 's/\([0-9]*\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/estacion3;\1\/2\/3/g' estacion
 sed 's/\([0-9]*\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/estacion4;\1\/2\/3/g' estacion4.csv > estacion4a.csv
 # Consolidacion de archivos estaciones
 touch estacionestotal.csv
-cat estacion1a.csv estacion2a.csv estacion3a.csv estacion4a.csv > estacionestotal.csv 
-# Instalacion libreria SQL
-sudo apt-get install -y csvkit  
+cat estacion1a.csv estacion2a.csv estacion3a.csv estacion4a.csv > estacionestotal.csv  
 # Cambio de "," por "."
 sed 's/,/./g' estacionestotal.csv > estacionestotal1.csv
 # Cambio de separadores ";" por ","
 sed 's/;/,/g' estacionestotal1.csv > estacionestotal2.csv
 # Cambio de "/" por "-"
 sed 's/\//-/g' estacionestotal2.csv > estacionestotal3.csv
-# Organizar formato mes y año
+# Organizar formato año AAAA
 sed 's/-\([0-9]*\),/-0\1,/g' estacionestotal3.csv > estacionestotal4.csv
 sed 's/-\([0-9]*\),/-20\1,/g' estacionestotal4.csv > estacionestotal5.csv
+# Organizar formato mes MM
 sed 's/-\([0-9]*\)-/-0\1-/g' estacionestotal5.csv > estacionestotal6.csv
-# Nombres a columnas
-sed 's/FECHA,/ESTACION,FECHA,AÑO,MES,HORA,/g' estacionestotal6.csv > estacionestotal7.csv
-# Creacion de variables año, mes y hora
+# Cambiar - por , (para separar por columnas)
+sed 's/-/,/g' estacionestotal6.csv > estacionestotal7.csv
+# Nombrar columnas
+sed 's/FECHA,/ESTACION,DIA,MES,YEAR,/g' estacionestotal7.csv > estacionestotal8.csv
+# Consultas SQL
+csvsql --query 'select ESTACION, MES, avg(VEL) from estacionestotal8 GROUP BY ESTACION, MES' estacionestotal8.csv >> velocidad-por-mes.csv #
+csvsql --query 'select ESTACION, YEAR, avg(VEL) from estacionestotal8 GROUP BY ESTACION, MES' estacionestotal8.csv >> velocidad-por-ano.csv #
+csvsql --query 'select ESTACION, HHMMSS, avg(VEL) from estacionestotal8 GROUP BY ESTACION, MES' estacionestotal8.csv >> velocidad-por-hora.csv #
+rm estacion*
+head velocidad-por-mes.csv
+head velocidad-por-ano.csv
+head velocidad-por-hora.csv
